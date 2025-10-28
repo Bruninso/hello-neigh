05263c6c40b04f52fafd12a1534b3a5da02eab6b
Bruno Sousa <bruno.ted9@outlook.com>
2025-10-28 12:33:11 -0300

    reformulação de todos os htmls tirando o index

--- backend/models/visitante.js
+++ backend/models/visitante.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const visitanteSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   cpf: { type: String, required: true, unique: true },
-  visitado: { type: String, required: true }, // Nome do morador visitado
+  visitado: { type: String, required: true }, 
   bloco: { type: String, required: true },
   apartamento: { type: String, required: true },
-  dataRegistro: { type: Date, default: Date.now } // Quando foi registrado
+  dataRegistro: { type: Date, default: Date.now } 
 });
 
 module.exports = mongoose.model('Visitante', visitanteSchema);
